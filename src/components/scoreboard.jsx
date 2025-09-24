cbb089e3733279651b6374d98c816a8cc7c44feb
shangwang <wangshangming10@gmail.com>
2025-09-24 09:37:52 -0400

    updated scoreboard style

--- src/components/scoreboard.jsx
+++ src/components/scoreboard.jsx
@@ -3,12 +3,25 @@ import { useState, useEffect } from "react";
 
 export default function ScoreBoard ({scoreCard}) {
     //where the scoreboard goes
-    const [score, setScore] = useState({currentScore: 0, highScore: 0})
+    // const [score, setScore] = useState({currentScore: 0, highScore: 0})
 
     return (
         <> 
-        <div className="score">Score: {scoreCard.current}</div> 
-        <div className="score">High score: {scoreCard.high}</div>
+        {/* <div className="score"><h1>Score: {scoreCard.current}</h1></div>  */}
+        <div className="horizontal-container">
+            <div class="final-score-wrapper gradient-2">
+                <p class="final-score white-text">{scoreCard.current}</p>
+                <p class="light-grey-text">of 12</p>
+            </div>
+            <div class="high-score-wrapper gradient-2">
+                <p class="final-score gold-text">{scoreCard.high}</p>
+                <p class="light-grey-text">High Score</p>
+                
+            </div>
+        </div>
+{/*         
+
+        <div className="score"><h1>High score: {scoreCard.high}</h1></div> */}
         </>
     )
 }
