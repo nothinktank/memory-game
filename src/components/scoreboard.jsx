900ba39b34a4ae6092b77d3adc2d8372fa6d0001
shangwang <wangshangming10@gmail.com>
2025-09-27 17:46:05 -0400

    adjusted pill position

--- src/components/scoreboard.jsx
+++ src/components/scoreboard.jsx
@@ -11,22 +11,11 @@ import { faCrown } from '@fortawesome/free-solid-svg-icons'
 // library.add(fas, fad, fadt);
 
 export default function ScoreBoard ({scoreCard}) {
-    //where the scoreboard goes
-    // const [score, setScore] = useState({currentScore: 0, highScore: 0})
-
+    
     return (
         <> 
-        {/* <div className="score"><h1>Score: {scoreCard.current}</h1></div>  */}
         <div className="horizontal-container">
-            {/* <div class="final-score-wrapper gradient-2">
-                <p class="final-score white-text">{scoreCard.current}</p>
-                <p class="light-grey-text">of 12</p>
-            </div>
-            <div class="high-score-wrapper gradient-2">
-                <p class="final-score gold-text">{scoreCard.high}</p>
-                <p class="light-grey-text">High Score</p>
-                
-            </div> */}
+            
             
             <div className="pill">
                 <p class="final-score white-text">{scoreCard.current} / 12 Hits</p>
